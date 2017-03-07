a50a60c295630403ea4e1f1fc3d99dfe8e7617f4
tmciver-maxwell <tim.mciver@maxwellhealth.com>
2017-03-07 16:07:48 -0500

    Upgrade to emacs 25.1 and add `typescript` mode.

--- init.el
+++ init.el
@@ -24,3 +24,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js-indent-level 8)
+ '(package-selected-packages
+   (quote
+    (typescript smartparens paredit neotree haskell-mode fill-column-indicator clojure-mode auto-complete))))
