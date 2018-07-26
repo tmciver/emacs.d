eb4b85963162017294b39257912eba22f0bd7b31
Tim McIver <tim@timmciver.com>
2018-07-26 15:27:36 -0400

    Add scala-mode from melpa.

--- init.el
+++ init.el
@@ -34,5 +34,5 @@
  '(markdown-command "pandoc")
  '(package-selected-packages
    (quote
-    (yasnippet-snippets yasnippet helm-ag projectile markdown-mode flycheck php-mode typescript smartparens paredit neotree haskell-mode fill-column-indicator clojure-mode auto-complete)))
+    (scala-mode yasnippet-snippets yasnippet helm-ag projectile markdown-mode flycheck php-mode typescript smartparens paredit neotree haskell-mode fill-column-indicator clojure-mode auto-complete)))
  '(tool-bar-mode nil))
