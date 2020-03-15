10808e43bd55ed38b74aca644f4ba0ba9ccd339e
Tim McIver <tim@timmciver.com>
2020-03-15 09:49:57 -0400

    Generate TAGS on save in haskell-mode.

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(haskell-tags-on-save t)
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
  '(markdown-command "pandoc")
