a2bc62bc2bf97d390da1dd25dfac0cc6ab513914
Tim McIver <tim@timmciver.com>
2020-05-01 15:23:50 -0400

    Make stack-ghci the haskell process type.

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(haskell-process-type (quote stack-ghci))
  '(haskell-tags-on-save t)
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
