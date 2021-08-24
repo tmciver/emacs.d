3346ad4df1363c0e83b28f37f6b7a940e1b83f4c
Tim McIver <tim@timmciver.com>
2021-08-24 17:38:36 -0400

    Remove call to `package-initialize` that was causing a warning.

--- init.el
+++ init.el
@@ -1,10 +1,3 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (require 'org)
 (org-babel-load-file "~/.emacs.d/configuration.org")
 ;; (custom-set-variables
