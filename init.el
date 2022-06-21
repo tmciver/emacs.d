8faf50bc9b937f153b6f2ef54a1c9f6b385d8e5d
Tim McIver <tmciver@mackeycorp.com>
2022-06-21 09:08:16 -0400

    Set up Elm LS.
    Squashed commit of the following:
    
    commit 4e93f24d388b00fb0f9b33419a846deb1b420dca
    Author: Tim McIver <tmciver@mackeycorp.com>
    Date:   Thu May 26 09:36:29 2022 -0400
    
        Start LSP when entering elm-mode.
    
    commit 2ee09114a144162e630c70eb23d5e1f0b79405cb
    Author: Tim McIver <tmciver@mackeycorp.com>
    Date:   Thu May 26 09:36:10 2022 -0400
    
        Add ability to scroll without moving cursor.
    
    commit 5c688d0d27e93c1fb48c400db44d3b1db791b04b
    Author: Tim McIver <tmciver@mackeycorp.com>
    Date:   Mon Mar 28 11:05:25 2022 -0400
    
        Some work towards getting elm-language-server working. Still doesn't work.

--- init.el
+++ init.el
@@ -11,12 +11,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(haskell-process-type (quote stack-ghci))
+ '(haskell-process-type 'stack-ghci)
  '(haskell-tags-on-save t)
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
  '(markdown-command "pandoc")
  '(package-selected-packages
-   (quote
-    (ivy lsp-haskell lsp-treemacs dot-mode nginx-mode pony-mode helm-projectile docker-compose-mode dockerfile-mode company-lsp treemacs lsp-ui dash lsp-mode jasminejs-mode company tide web-mode dumb-jump counsel angular-mode angular-snippets typescript-mode scala-mode yasnippet-snippets yasnippet helm-ag projectile markdown-mode flycheck php-mode paredit neotree haskell-mode fill-column-indicator clojure-mode auto-complete)))
+   '(exec-path-from-shell tree-sitter tree-sitter-indent tree-sitter-langs ivy lsp-haskell lsp-treemacs dot-mode nginx-mode pony-mode helm-projectile docker-compose-mode dockerfile-mode company-lsp treemacs lsp-ui dash lsp-mode jasminejs-mode company tide web-mode dumb-jump counsel angular-mode angular-snippets typescript-mode scala-mode yasnippet-snippets yasnippet helm-ag projectile markdown-mode flycheck php-mode paredit neotree haskell-mode fill-column-indicator clojure-mode auto-complete))
  '(tool-bar-mode nil))
