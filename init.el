0574bf8d8b013e79814601d322aa13162d0d4117
Tim McIver <tmciver@mackeycorp.com>
2023-05-04 16:10:13 -0400

    Add projectile-ripgrep.

--- init.el
+++ init.el
@@ -12,10 +12,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(haskell-process-type 'stack-ghci)
+ '(haskell-stylish-on-save t)
  '(haskell-tags-on-save t)
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
  '(markdown-command "pandoc")
  '(package-selected-packages
-   '(sparql-mode ttl-mode exec-path-from-shell tree-sitter tree-sitter-indent tree-sitter-langs ivy lsp-haskell lsp-treemacs dot-mode nginx-mode pony-mode helm-projectile docker-compose-mode dockerfile-mode company-lsp treemacs lsp-ui dash lsp-mode jasminejs-mode company tide web-mode dumb-jump counsel angular-mode angular-snippets typescript-mode scala-mode yasnippet-snippets yasnippet helm-ag projectile markdown-mode php-mode paredit neotree haskell-mode fill-column-indicator clojure-mode auto-complete))
+   '(projectile-ripgrep ripgrep sparql-mode ttl-mode exec-path-from-shell tree-sitter tree-sitter-indent tree-sitter-langs ivy lsp-haskell lsp-treemacs dot-mode nginx-mode pony-mode helm-projectile docker-compose-mode dockerfile-mode company-lsp treemacs lsp-ui dash lsp-mode jasminejs-mode company tide web-mode dumb-jump counsel angular-mode angular-snippets typescript-mode scala-mode yasnippet-snippets yasnippet helm-ag projectile markdown-mode php-mode paredit neotree haskell-mode fill-column-indicator clojure-mode auto-complete))
  '(tool-bar-mode nil))
