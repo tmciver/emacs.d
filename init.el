84a56dfbf7aae3562168023faff976bb24cb18cb
Tim McIver <tim@timmciver.com>
2025-05-27 17:32:16 -0400

    Disable LSP. Use TAGS for jump-to-definition functionality.

--- init.el
+++ init.el
@@ -11,11 +11,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(haskell-process-type 'stack-ghci)
+ '(haskell-process-type 'cabal-repl)
  '(haskell-tags-on-save t)
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
  '(markdown-command "pandoc")
  '(package-selected-packages
-   '(direnv sparql-mode ttl-mode exec-path-from-shell tree-sitter tree-sitter-indent tree-sitter-langs ivy lsp-haskell lsp-treemacs dot-mode nginx-mode pony-mode helm-projectile docker-compose-mode dockerfile-mode company-lsp treemacs lsp-ui dash lsp-mode jasminejs-mode company tide web-mode dumb-jump counsel angular-mode angular-snippets typescript-mode scala-mode yasnippet-snippets yasnippet helm-ag projectile markdown-mode php-mode paredit neotree haskell-mode fill-column-indicator clojure-mode auto-complete))
+   '(ace-jump-mode auto-complete clojure-mode dante direnv dockerfile-mode elm-mode
+                   exec-path-from-shell fill-column-indicator git-commit
+                   graphviz-dot-mode haskell-mode htmlize ivy lsp-haskell
+                   lsp-java lsp-python-ms lsp-ui magit multiple-cursors nix-mode
+                   paredit php-mode pkg-info purescript-mode smartparens
+                   sparql-mode treemacs-projectile ttl-mode use-package web-mode
+                   which-key yaml-mode yasnippet))
  '(tool-bar-mode nil))
